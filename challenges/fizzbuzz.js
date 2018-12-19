690addf19a0cc4ce589f2b034c43dead78a5552d
joel burton <joeltburton@gmail.com>
2018-12-19 09:33:38 -0800

    Completed hack hour challenge.

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,33 @@
 //                     16 ]
 
 function fizzbuzz(num) {
+  if (typeof num !== 'number' || num < 1) console.error('Please enter a number greater than zero!');
+  const output = [];
 
+  // loop over numbers from 1 to num
+  for (let n = 1; n <= num; n += 1) {
+    // initialize an empty string to build fizzbuzz
+    let string = '';
+
+    // if current number divisible by 3 string += 'fizz'
+    if (n % 3 === 0) string += 'fizz';
+    // if current number divisible by 5 string += 'buzz'
+    if (n % 5 === 0) string += 'buzz';
+    // if string is empty --> current number not divisble by 3 or 5
+    // push current number to output,
+    // otherwise push string to output
+    string ? output.push(string) : output.push(n);
+  }
+  // after loop, return output;
+  return output;
 }
+// ||||||     FIZZBUZZ TESTS	     ||||||
+// console.log({ 'num = 30': fizzbuzz(30) });
+// console.log({ 'num = 5': fizzbuzz(5) });
+// console.log({ 'num = 305': fizzbuzz(305) });
+// console.log({ 'num = -30': fizzbuzz(-30) });
+// console.log({ 'num = 0': fizzbuzz(0) });
+// console.log({ "num = 'string'": fizzbuzz('string') });
+
 
 module.exports = fizzbuzz;
