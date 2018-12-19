76a0d8aab8382868bb7521a21cb9221b81d5dc02
bradenaa <bradenaltstatt@gmail.com>
2018-12-19 09:34:14 -0800

    completed the fizzbuzz challenge

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,24 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+  if (num < 0) {
+    return [];
+  }
+  // declare a const, assign it an empty array
+  const result = [];
+  // increment by one up to num, starting at 1
+  for (let i = 1; i <= num; i += 1) {
+    // if divisible by three and five, push 'fizzbuzz' to the array
+    // if number is divisible by three then push 'fizz' to array
+    // if num is divisible by five then push 'buzz ' to the array
+    const isDiv3 = i % 3 === 0;
+    const isDiv5 = i % 5 === 0;
+    if (isDiv3 && isDiv5) result.push('fizzbuzz');
+    else if (isDiv3) result.push('fizz');
+    else if (isDiv5) result.push('buzz');
+    else result.push(i);
+  }
+  return result;
 }
 
 module.exports = fizzbuzz;
