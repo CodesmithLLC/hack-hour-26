f14cebf69a8cdd32469b35374c91dc930a34a037
BENJAMIN ZACK WOODSON <ben.woodson@gmail.com>
2018-12-20 09:54:04 -0800

    "approach worked out, implementation broken"

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -13,9 +13,33 @@
 
 */
 
-function drawStairs(n) {
+/**
+   I: num
+   O: a printout of asterisks (string) arranged in an isoseles triangle where the base is the length in asterisks
+   strict equals num and the top (last printout) equals one asterisk
+ * 
+ * 
+ * / */
 
-}
+
+ function drawStairs(n) {
+    // iterate
+    //Array()
+    let step = Array(7).fill(' ');
+    //console.log(step.length)
+    for (let i = step[step.length - 1]; i > 0; i -= 1) {
+      step.pop();
+      step.push('*')
+      console.log(step.join(''))
+    }
+    
+      // init var at 1
+      // stop condition < equal to n
+      // increment var
+   // ea iteration, add 1
+  
+   //
+  }
 
 
 module.exports = drawStairs;
