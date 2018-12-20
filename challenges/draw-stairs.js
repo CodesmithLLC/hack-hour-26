38f4ef44b54380976b0e6d2a8cc1feb6312b2aab
trischoe <44879418+trischoe@users.noreply.github.com>
2018-12-20 09:44:06 -0800

    draw-stairs approach implemented

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,18 @@
 */
 
 function drawStairs(n) {
+    let stairs = '';
+    let i = n;
+    for(n; n > 0; n--){
+        stairs += ' '.repeat(n-1)+'*'.repeat(i-n+1);
+        if(n !== 1){
+            stairs += '\n';
+        }
+    }
+    return stairs;
 
 }
 
 
 module.exports = drawStairs;
+
