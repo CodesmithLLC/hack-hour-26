5942a968549ef25640f176c396f1aac95c151cc4
p <petulacode@gmail.com>
2018-12-20 11:25:32 -0800

    incomplete solution

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,7 +14,22 @@
 */
 
 function drawStairs(n) {
-
+//expected result is a staircase with vertical height = n and horizontal steps = n
+//each vertical step will also extend step to left by n 
+//strategy: 
+  //use 2 nested for loops to iterate, initializing at 1 and stopping when i < n
+  //first loop will be for vertical 
+  //second for loop will be for horizontal to add to left  
+  //before breaking out of second for loop, console log result
+  //when i === n console log a line of asterisks
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {  
+      console.log('*', '*')//j at i number of times and to the left); 
+      }
+      if (i === null) {
+          console.log() //how to write code to print * i number of times; 
+      }
+  }
 }
 
 
