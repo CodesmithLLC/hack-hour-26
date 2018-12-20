7f0ae541241273e585f9a242d6da3452416d4552
jordanzobean <jordanbetzer@gmail.com>
2018-12-20 09:44:20 -0800

    finished staircase challenge

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,23 @@
 */
 
 function drawStairs(n) {
-
+  let numberOfSpaces = n - 1; // number of initial spaces before first stair
+  let stair = '*'; // initial number of stairs to print
+  let space = ' '; // var to store spaces
+  for (let i = 0; i < n; i++) {
+    // outer loop loops n number of times (height and width of staircase)
+    let stairCase = ''; // initialize a new empty level of stairs
+    for (let j = 0; j < numberOfSpaces; j++) {
+      // loop to print correct number of spaces
+      stairCase += space; // adds spaces to staircase string
+    }
+    numberOfSpaces -= 1; // after adding spaces, decrement number of spaces to add by 1
+    stairCase += stair; // add a stair to stairCase string
+    stair += '*'; // increment number of stairs to add in next loop by 1
+    console.log(stairCase); // log current level of stairs
+  }
 }
 
+drawStairs(6);
 
 module.exports = drawStairs;
