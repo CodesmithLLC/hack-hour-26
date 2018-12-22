41741a4c534ec0a9d5cf0068412273d8029e0f75
p <petulacode@gmail.com>
2018-12-22 10:03:59 -0800

    did not complete node challenge

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -21,8 +21,14 @@ function Node(val) {
   this.next = null;
 }
 
-function kthToLastNode(k, head) {
+//input: two parameters - an integer identifying index position of node and the head of linked list; 
+//output: returns the node value of the node that is positioned at final node - identified node
 
+//use Object.values to access value
+
+function kthToLastNode(k, head) {
+//let tail = head.next; this will re-assign head to the next node; how to get tail to reference the final node in the list? 
+//when value of tail === null i.e. when tail is pointing to null then return Object.values(tail) to get the value
 }
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
