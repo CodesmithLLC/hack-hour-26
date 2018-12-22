f7c305c82b77b5962fd251a1e3524c304f3fe0a2
BENJAMIN ZACK WOODSON <ben.woodson@gmail.com>
2018-12-22 10:03:40 -0800

    "first attempt - logic dubious"

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -16,13 +16,33 @@
  * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
  */
 
+ /**
+  * I: P1) int representing distance from last node, P2) head of LL
+  * O: val at kth to last node in LL
+  */
+
+console.log('kth!');
+
 function Node(val) {
   this.value = val;
   this.next = null;
 }
 
-function kthToLastNode(k, head) {
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
 
+function kthToLastNode(k, head) {
+  let map = {};
+  let count = 0;
+  for (let i = this.head; i.next !== null; i = this.next) {
+    count += 1;
+    map[count] = this.value;
+  }
+
+  return map[k];
 }
 
-module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+//module.exports = {Node: Node, kthToLastNode: kthToLastNode};
