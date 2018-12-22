c679b00648d7914f1117210080e3c88d8f65d69a
joel burton <joeltburton@gmail.com>
2018-12-22 09:48:40 -0800

    Completed kth-to-last-node challenge. Handled edge cases of k < 1 and k < greater than list length.

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,45 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  // need to check if n > 0;
+  if (k < 1) return;
+  const values = [];
+  let currentNode = head;
+  // need to traverse the list, storing values of each node in an array
+  while (currentNode.next) {
+    values.push(currentNode.value);
+    currentNode = currentNode.next;
+  }
+  // while loop quits before the value of the tail is pushed, so do that next
+  values.push(currentNode.value);
+  // console.log(values);
 
+  // once the end is reached, grab length and calculate the index in the array of the kth item (length - k)
+  //    check if k is going to be greater than or equal the length
+  const { length } = values;
+  if (k > length) return undefined;
+
+  // return kth item
+  return values[length - k];
 }
 
-module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E - I\'m last!');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+
+console.log('k = 0, expect undefined, actual:', kthToLastNode(0, a));
+console.log('k = 1, expect E - I\'m last!, actual:', kthToLastNode(1, a));
+console.log('k = 2, expect D, actual:', kthToLastNode(2, a));
+console.log('k = list length, expect A, actual:', kthToLastNode(5, a));
+console.log('k = list length + 1, expect undefined, actual:', kthToLastNode(6, a));
+
+
+module.exports = { Node, kthToLastNode };
