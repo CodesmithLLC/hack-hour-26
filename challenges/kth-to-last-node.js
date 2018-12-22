063c12c03c6420ec8c725de17e3a017ce927f515
Chang Yea Moon <changyeamoon@gmail.com>
2018-12-22 09:30:34 -0800

    completed brute force kth

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,39 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
-
+  let result;
+  let curr = head;
+  let length = 0;
+  while (curr !== null) {
+    length += 1;
+    curr = curr.next;
+  }
+  let stop = ((length - k) >= 0) ? (length - k) : (length - k) + length;
+  // console.log(stop);
+  curr = head;
+  while (stop >= 0) {
+    result = curr.value;
+    curr = curr.next;
+    stop -= 1;
+  }
+  return result;
 }
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
 
-module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+console.log(kthToLastNode(2, a));
+console.log(kthToLastNode(3, a));
+console.log(kthToLastNode(4, a));
+console.log(kthToLastNode(5, a));
+console.log(kthToLastNode(6, a));
+
+
+module.exports = { Node, kthToLastNode };
