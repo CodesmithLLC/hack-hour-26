30035faf2fbf661f8118a8a4cddc1f084afbbc86
Melody <melodychai2@gmail.com>
2018-12-22 09:36:51 -0800

    finished k to last node

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,37 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  if(!head) return undefined;
+  if(!head.next) return head;
+  if(k < 1) return undefined;
+  let nodeTracker = []; // keeps track of k nodes
+  let count = 0;
+  let curr = head;
+  while(curr){
+    count++;
+    nodeTracker.push(curr);
+    if(nodeTracker.length > k) nodeTracker.shift();
+    curr = curr.next;
+  }
+  console.log(nodeTracker)
+  if(count < k) return "K value larger than linked list size";
+  return nodeTracker[0].value;
+
 
 }
+/*
+ const a = new Node('A');
+ const b = new Node('B');
+ const c = new Node('C');
+ const d = new Node('D');
+ const e = new Node('E');
+
+ a.next = b;
+ b.next = c;
+ c.next = d;
+ d.next = e;
+
+ console.log(kthToLastNode(6, a));
+ */
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
