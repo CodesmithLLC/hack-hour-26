6f95f41f3a097da99e8dd157f099d67d021be97b
codebyjmd <StudiousMusic@JMD.local>
2018-12-26 09:35:36 -0800

    pow solution J Domovitch

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,15 @@
  */
 
 function pow(base, power) {
-
+  if (power === 0) {
+    return 1;
+  } else if (power === 1) {
+    return base;
+  }
+  return base * Math.pow(base, power - 1);
 }
+
+console.log(pow(2, 3));
+
 
 module.exports = pow;
