158a34c7983635b35899404decce9196d546af81
Jonathan Mavandi <jonathan.mavandi@gmail.com>
2018-12-26 09:35:32 -0800

    pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,10 +3,17 @@
  */
 
 function pow(base, power) {
-    if (power === 1) return base ;
-    return base * pow(base, power -1)
+    if (power === 0) return 1; // if power is 0, always equal to 1.
+    if (power === 1) return base;
+    if (power === -1) return (1 / base); // working with negative exponents
+    if (power > 1) {
+       return base * pow(base, power -1);
+    };
+    if (power < 1) {
+        return base * pow(base, power +1);
+    }
 }
 
 console.log(pow(2,3));
-
+// console.log(pow(2,-4));
 module.exports = pow;
