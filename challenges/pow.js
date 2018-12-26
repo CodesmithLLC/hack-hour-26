b0e7ab4d911e3390f1f7eb938d5ea7e448e72d9c
jordanzobean <jordanbetzer@gmail.com>
2018-12-26 09:20:06 -0800

    finished POW

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,14 @@
  */
 
 function pow(base, power) {
-
+  // special case: base and power are not a number
+  if (typeof base !== 'number' && typeof power !== 'number') return undefined;
+  // base case: power === 0
+  if (power === 1) return base;
+  // recurse passing in base and power - 1
+  return base * pow(base, power - 1);
 }
+
+console.log(pow(2, 3)); // 2^3 = 8;
 
 module.exports = pow;
