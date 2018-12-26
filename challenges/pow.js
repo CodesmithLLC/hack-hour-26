72bd76b26cd7e32bbb1385bd57a44197f24063e7
Chang Yea Moon <changyeamoon@gmail.com>
2018-12-26 09:51:41 -0800

    Accounted for negative edge cases

--- challenges/pow.js
+++ challenges/pow.js
@@ -5,17 +5,22 @@
 function pow(base, power, result = 1) {
   // tail call recursion
   if (power === 0) return result;
-  return pow(base, power - 1, result * base);
+  if (power > 0) {
+    return pow(base, power - 1, result * base);
+  }
+  if (power < 0) {
+    return pow(base, power + 1, result / base);
+  }
   // linear recursion
 //   if (power === 0) return 1;
 //   return base * pow(base, power - 1);
 }
 
-console.log(pow(2, 0)); // should equal 1
-console.log(pow(2, 1)); // should equal 2
-console.log(pow(2, 2)); // should equal 4
-console.log(pow(3, 1)); // should equal 3
-console.log(pow(2, 400)); // should equal 1
+// console.log(pow(2, 0)); // should equal 1
+// console.log(pow(2, 1)); // should equal 2
+// console.log(pow(2, 2)); // should equal 4
+// console.log(pow(3, 1)); // should equal 3
+// console.log(pow(2, -2)); // should equal 1
 
 
 module.exports = pow;
