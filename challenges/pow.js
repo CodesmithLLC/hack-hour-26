ca6256e94bdcd7149723ae042e8759c09a3e0b2f
JayCogen44 <jaycog44@gmail.com>
2018-12-26 09:39:00 -0800

    finished challege and all edge cases

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,24 @@
  */
 
 function pow(base, power) {
-
+  const isNegative = base < 0 ? true : false;
+  if (base === 0) return 0;
+  if (power === 0 && base >= 0) return 1;
+  if (power === 0 && base < 0) return -1;
+  if (power === -1) return 1 / base;
+  if (power === 1) return base;
+  if (power < 0) {
+    return isNegative
+      ? -1 * (pow(base, power + 1) / base)
+      : pow(base, power + 1) / base;
+  }
+  if (power > 0) {
+    return isNegative
+      ? -1 * base * pow(base, power - 1)
+      : base * pow(base, power - 1);
+  }
 }
+
+console.log(pow(-2, 7));
 
 module.exports = pow;
