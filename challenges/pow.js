170d0f2beb3ba6f67ed7cae7f96c833fd34cf5de
emiliacarmel <emiliacarmel@gmail.com>
2018-12-26 09:32:17 -0800

    pow hack attempt

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,16 @@
  */
 
 function pow(base, power) {
-
+  // guard clause 1 - if base === 0 && power === 0, return undefined;
+  if (base === 0 && power === 0) return undefined;
+  // guard clause 2 - if base === 0, return 0;
+  if (base === 0) return 0;
+  // guard clause 3 - if power === 0, return 1;
+  if (power === 0) return 1;
+  // base case - if power === 1, return base * power
+  if (power === 1) return base * power;
+  // else recursively call base * pow(base * power - 1)
+  return base * pow(base, power -= 1);
 }
 
 module.exports = pow;
