740ec6043843eb4ff5a9ccbf695743eefb3268d8
Byron Inocencio <jay.byron@gmail.com>
2018-12-26 09:58:14 -0800

    finished positives for pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,42 @@
  * Use recursion!
  */
 
+
+ /*brute forcer power function
 function pow(base, power) {
 
+  if (power === 0){
+    return 1;
+  }
+
+  return base * pow(base, power -1);
+
+}*/
+
+
+
+function pow(base,power){
+  let x;
+  if(power===0){
+    return 1;
+  }
+  else if (power%2 ===0) {
+      x= pow(base,parseInt(power/2,10))*pow(base,parseInt(power/2,10));
+  }
+  
+  else {
+       x = base*pow(base,parseInt(power/2,10))*pow(base,parseInt(power/2,10));
+  }
+
+  if(power<0){
+    return (1/x);
+  }
+
+  else{
+    return x;
+  }
 }
+
+console.log(pow(3,-2));
 
 module.exports = pow;
