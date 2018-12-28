cb54818d79b886d7d537f39060925ce19f913d18
joel burton <joeltburton@gmail.com>
2018-12-28 09:50:00 -0800

    Completed Reverse in Place. Handles empty arrays, arrays with 1 element, odd numbers of elements, and even numbers of elements

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -1,4 +1,5 @@
-'use strict';
+
+
 /**
  * Write a function to reverse an array in place
  *
@@ -14,7 +15,38 @@
  */
 
 function reverseInPlace(array) {
+  // initiate two pointers, 0 and length - 1
+  let front = 0;
+  let back = array.length - 1;
 
+  // while first is smaller than last pointer
+  while (front < back) {
+    // grab element at each pointer
+    const fromFront = array[front];
+    const fromBack = array[back];
+
+    // place element in array at opposite pointer
+    array[back] = fromFront;
+    array[front] = fromBack;
+
+    // increment/decrement pointers
+    front += 1;
+    back -= 1;
+  }
+  return array;
 }
+
+// * Testing * //
+
+// const empty = [];
+// const single = [0];
+// const odd = [0, 1, 2, 3, 4];
+// const even = [0, 1, 2, 3, 4, 5];
+
+// console.log('[] => []', reverseInPlace(empty));
+// console.log('[0] => [0]', reverseInPlace(single));
+// console.log('[0, 1, 2, 3, 4] => [4, 3, 2, 1, 0]', reverseInPlace(odd));
+// console.log('[0, 1, 2, 3, 4, 5] => [5, 4, 3, 2, 1, 0]', reverseInPlace(even));
+
 
 module.exports = reverseInPlace;
