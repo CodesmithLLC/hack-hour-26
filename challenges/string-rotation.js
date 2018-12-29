f7b337bf9730159aa1d0f3c87fac377f949108a3
joel burton <joeltburton@gmail.com>
2018-12-29 09:55:35 -0800

    I think I did it. Not totally sure of the end goal of only calling isSubstring once. Can be done without calling isSubstring at all.

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -11,12 +11,44 @@
  *              stringRotation("hello", "ollhe") -> false (not a rotation, just an anagram)
  */
 
+// ? I'm not sure if I understand the goal of calling isSubstring only once...
+
 function isSubstring(s1, s2) {
   return s1.indexOf(s2) >= 0;
 }
 
-function stringRotation(s1, s2) {
-
+function rotate(string) {
+  newString = string.slice(1);
+  return newString + string[0];
 }
 
-module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
+function stringRotation(s1, s2) {
+  // strings need to be the same length and of type string
+  if (typeof s1 !== 'string' || typeof s2 !== 'string') return false;
+  if (s1.length !== s2.length) return false;
+  if (s1.length === 0 && s2.length === 0) return true;
+
+  let counter = 0;
+  let rotating = s2;
+  while (counter < s1.length) {
+    if (s1 === rotating) return isSubstring(s1, rotating);
+    // if (s1 === rotating) return true;
+    rotating = rotate(rotating);
+    counter += 1;
+  }
+  return false;
+}
+
+module.exports = { isSubstring, stringRotation };
+
+// * TESTING * //
+
+// console.log(`typeof '' ${typeof ''}`);
+// console.log(`'' & '' -> true : ${stringRotation('', '')}`);
+// console.log(`' ' & ' ' -> true : ${stringRotation(' ', ' ')}`);
+// console.log(`'1' & '12' -> false : ${stringRotation('1', '12')}`);
+// console.log(`'1234' & '4123' -> true : ${stringRotation('123', '312')}`);
+// console.log(`'1234' & '2341' -> true : ${stringRotation('123', '312')}`);
+// console.log(`'1234' & '' -> false : ${stringRotation('1234', '')}`);
+// console.log(`[] & '' -> undefined : ${stringRotation([], '')}`);
+// console.log(`'yeah boiiii' & 'boiiiiyeah ' -> true : ${stringRotation('', '')}`);
