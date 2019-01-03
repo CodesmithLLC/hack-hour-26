d86d4a6f67dd891fac80c68444e65f5791f53542
trischoe <tristans7@gmail.com>
2019-01-03 09:43:28 -0800

    roman numeral approach

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,6 +18,63 @@
  */
 
 function romanNumeral(n) {
+    let romanNum = '';
+
+    while(n > 0 ){
+        while(n >= 1000){
+            romanNum += 'M';
+            n -= 1000;
+        }
+        while(n >= 900){
+            romanNum += 'CM';
+            n -= 900;
+        }
+        while(n >= 500){
+            romanNum += 'D';
+            n -= 500;
+        }
+        while(n >= 400){
+            romanNum += 'CD';
+            n -= 400;
+        }
+        while(n >= 100){
+            romanNum += 'C';
+            n -= 100;
+        }
+        while(n >= 90){
+            romanNum += 'XC';
+            n -= 90;
+        }
+        while(n >= 50){
+            romanNum += 'L';
+            n -= 50;
+        }
+        while(n >= 40){
+            romanNum += 'XL';
+            n -= 40;
+        }
+        while(n >= 10){
+            romanNum += 'X';
+            n -= 10;
+        }
+        while(n >= 9){
+            romanNum += 'IX';
+            n -= 9;
+        }
+        while(n >= 5){
+            romanNum += 'V';
+            n -= 5;
+        }
+        while(n >= 4){
+            romanNum += 'IV';
+            n -= 4;
+        }
+        while(n >= 1){
+            romanNum += 'I';
+            n -=1;
+        }
+    }
+    return romanNum;
 
 }
 
