4e7dc14d03b2e7982ddf33caaacf7053bc53a039
jordanzobean <jordanbetzer@gmail.com>
2019-01-03 20:20:24 -0800

    finished balanced-parens and roman-numeral

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,43 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
 function romanNumeral(n) {
+  // create a table of primary roman numerals
+  const romans = {
+    1: 'I',
+    4: 'IV',
+    5: 'V',
+    9: 'IX',
+    10: 'X',
+    40: 'XL',
+    50: 'L',
+    90: 'XC',
+    100: 'C',
+    400: 'CD',
+    500: 'D',
+    900: 'CM',
+    1000: 'M'
+  };
 
+  let romanConvert = '';
+
+  romansArr = Object.entries(romans);
+  // console.log('romansArr: ', romansArr);
+
+  for (let i = romansArr.length - 1; i >= 0; i--) {
+    // Check if n is greater than or equal to a roman numeral
+    while (n >= romansArr[i][0]) {
+      romanConvert += romansArr[i][1];
+      n -= romansArr[i][0];
+    }
+  }
+
+  return romanConvert;
 }
+
+console.log(romanNumeral(48));
 
 module.exports = romanNumeral;
