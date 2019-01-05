5c746e0f3a9149da0b540e094d1bcc96de1a26ca
JayCogen44 <jaycog44@gmail.com>
2019-01-05 09:39:31 -0800

    updates to roman numerals

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -17,8 +17,56 @@
  * 
  */
 
-function romanNumeral(n) {
+romanNumerals = [
+    1000, 'M',
+    900, 'CM',
+    500, 'D',
+    400, 'CD',
+    100, 'C',
+    90, 'XC',
+    50, 'L',
+    40, 'XL',
+    10, 'X',
+    9, 'IX',
+    5, 'V',
+    4, 'IV',
+    1, 'I',
+]
 
+
+function romanNumeral(n) {
+    let workingNum = n;
+    let result = '';
+    for (let i = 0; i < romanNumerals.length; i += 2) {
+        while (workingNum >= romanNumerals[i]) {
+            result += romanNumerals[i + 1];
+            workingNum -= romanNumerals[i];
+        }
+    }
+    return result;
 }
+
+// romanNumeralsRec = [
+//     {1000: 'M'},
+//     {900: 'CM'},
+//     {500: 'D'},
+//     {400: 'CD'},
+//     {100: 'C'},
+//     {90: 'XC'},
+//     {50: 'L'},
+//     {40: 'XL'},
+//     {10: 'X'},
+//     {9: 'IX'},
+//     {5: 'V'},
+//     {4: 'IV'},
+//     {1: 'I'}
+// ]
+// function romanNumeralRec(n, result='') {
+//     if(n === 0) return result;
+
+//     romanNumeralFunc()
+// }
+
+console.log(romanNumeral(1909));
 
 module.exports = romanNumeral;
