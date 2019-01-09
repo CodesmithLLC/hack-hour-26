ca4d3748b82469619125349c50aeb68b53f991ec
Harmon Huynh <harmon.huynh@outlook.com>
2019-01-09 09:21:02 -0800

    Completed highest product js

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,9 +3,11 @@
  */
 
 function highestProduct(array) {
+  if (array.length < 3) return 0;
+  if (!Array.isArray(array)) return 0;
   const newArray = array.slice();
   newArray.sort((a, b) => {
-    return a - b;
+    return b - a;
   });
   return array[0] * array[1] * array[2];
 }
