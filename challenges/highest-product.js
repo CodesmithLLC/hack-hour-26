c163cf172628b51579fc708d8f2baf33722c58d3
Byron Inocencio <jay.byron@gmail.com>
2019-01-09 09:13:46 -0800

    completed highest product using sorting

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -4,6 +4,18 @@
 
 function highestProduct(array) {
 
+  const sorted = array.sort(function(a, b){return b-a});
+  
+
+  let product =1;
+  for(let i =0; i<2; i++){
+
+    product = product * [sorted[i]];
+  }
+
+  return product;
+
+
 }
 
 
