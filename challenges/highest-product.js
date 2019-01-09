fe4a02c98d3414fdc05d4aa924152d8debe3dd66
trischoe <tristans7@gmail.com>
2019-01-09 09:42:34 -0800

    highest-product approach

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,20 @@
  */
 
 function highestProduct(array) {
-
-}
+    array.sort(function(a, b) {
+      return a - b;
+    });
+  
+    if(array[0] > 0 && array.length >= 3){
+        return array.pop() * array.pop() * array.pop()
+    }
+    if(array.length >= 3){
+        if((array[0] * array[1] * array[length -1]) >= (array[length-1] * array[length-2] * array[length-3])){
+            return array[0] * array[1] * array[length -1]
+        }
+    }
+  
+  }
 
 
 module.exports = highestProduct;
