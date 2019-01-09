9e2ecd6c854fb93b2ec525dee1e96cea5eceddf4
JayCogen44 <jaycog44@gmail.com>
2019-01-09 09:27:07 -0800

    not elegant but i think i got this working

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,26 @@
  */
 
 function highestProduct(array) {
-
+    return array.reduce((top, e) => {
+        if (!top[0]) {
+            top.push(e);
+        } else if (e > top[0]) {
+            top.unshift(e);
+            if (top.length >= 3) top.pop();
+        } else if (!top[1]) {
+            top.push(e);
+        } else if (e > top[1]) {
+            top[1] = e;
+        } else if (!top[2]) {
+            top.push(e);
+        } else if (e > top[2]) {
+            top[2] = e;
+        }
+        return top;
+    }, [])
+        .reduce((product, e) => {
+            return product *= e;
+        });
 }
-
 
 module.exports = highestProduct;
