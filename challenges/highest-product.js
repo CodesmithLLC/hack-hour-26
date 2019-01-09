13f517d913aa98fe9b5c0e34541962401eaac95c
Melody <melodychai2@gmail.com>
2019-01-09 09:05:17 -0800

    finished highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,17 @@
  */
 
 function highestProduct(array) {
+  array.sort(sortAlg);
+  console.log(array);
+  return array[0]*array[1]*array[2];
 
 }
+
+function sortAlg(a,b){
+  return b-a;
+}
+let arr = [0,10,15,7,2];
+console.log(highestProduct(arr));
 
 
 module.exports = highestProduct;
