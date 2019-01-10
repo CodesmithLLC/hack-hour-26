bbaccedaa3bfb42af5872ae9e5193d0c28b13bcf
BENJAMIN ZACK WOODSON <ben.woodson@gmail.com>
2019-01-09 18:10:03 -0800

    next attempt with edge case handling  and memo corrected

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -17,25 +17,27 @@
  * 
  */
 const highestProduct = (arr) => {
-  // reduce arr -> memo -> highest: {first: 0, second: 0, third: 0},
+  if (!Array.isArray(arr) || arr.length < 3) return 0;
+
   const topThree = arr.reduce((highest, num) => {
+
     let first = highest.first;
     let second = highest.second;
     let third = highest.third;
 
     if (num >= highest.first) {
-      highest.second = first; 
+      highest.second = first;
       highest.third = second;
       highest.first = num;
-    } else if (num >= highest.second) { 
+    } else if (num >= highest.second) {
       highest.third = second;
       highest.second = num;
-    } else if (num >= highest.third) { 
+    } else if (num >= highest.third) {
       highest.third = num;
     }
 
     return highest;
-  }, { first: 0, second: 0, third: 0 });
+  }, { first: -Infinity, second: -Infinity, third: -Infinity });
 
   return topThree.first * topThree.second * topThree.third;
 };
