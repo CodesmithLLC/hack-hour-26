0ac935a27aab65cc8ae9d694577decb969d32c3b
bradenaa <bradenaltstatt@gmail.com>
2019-01-11 11:11:26 -0800

    completed zip-linked-lists to return a linked list that was azipper of the two lists, which started with the first list

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,27 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  // create a current position node for each 
+  let currentNodeList1 = l1.head;
+  let currentNodeList2 = l2.head;
+
+  // while the linked list next value is not null, then continue
+  while(currentNodeList1.next !== null) {
+    // temporarily store the next value of the currentNode
+    let temp = currentNodeList1.next;
+    // create a new node of list2 current value
+    const toInsert = new Node(currentNodeList2.value);
+    // change the next value to be the old next value
+    toInsert.next = temp;
+    // insert the new node to be the next value of the curre
+    currentNodeList1.next = toInsert;
+    // change the currentNode to be the 
+    currentNodeList1 = currentNodeList1.next.next;
+    currentNodeList2 = currentNodeList2.next;
+  }
+  l1.tail.next = l2.tail
+  return l1;
 };
+
 
 module.exports = {Node: Node, zip: zip};
