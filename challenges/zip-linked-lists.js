b83044e159ce7a2e1ac3a40de583f68f91bdf25c
Harmon Huynh <harmon.huynh@outlook.com>
2019-01-11 10:16:51 -0800

    Updated zip linked list

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,14 +11,21 @@ function Node(val) {
 }
 
 function zip(l1, l2, l3 = new Node(null), s = 0) {
-  if (l1 === undefined) return l2;
-  if (l2 === undefined) return l1;
+  if (l1 === undefined || l2 === undefined) return l2 || l1;
   if (l1.value === null || l2.value === null) return l3;
   if (l3.value === null) {
     l3.value === l1.value;
     s = 1;
   }
-  return s === 0 ? zip(l1.next, l2, l3.next = new Node(l1.value), s = 1) : zip(l1, l2.next, l3.next = new Node(l2.value), s = 0);
+  if (s = 0) {
+    l3.next = l1.value;
+    return zip(l1.next, l2, l3.next, s = 1);
+  }
+  if (s = 1) {
+    l3.next = l2.value;
+    return zip(l1, l2.next, l3.next, s = 0);
+  }
+  // return s === 0 ? zip(l1.next, l2, l3.next = new Node(l1.value), s = 1) : zip(l1, l2.next, l3.next = new Node(l2.value), s = 0);
 };
 
 module.exports = { Node: Node, zip: zip };
