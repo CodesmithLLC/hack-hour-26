2594d375f38c1b82000a4d655d17f48614ae0240
Harmon Huynh <harmon.huynh@outlook.com>
2019-01-11 09:48:35 -0800

    Completed zip linked list

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,7 +10,13 @@ function Node(val) {
   this.next = null;
 }
 
-function zip(l1, l2) {
+function zip(l1, l2, l3 = new Node(null), s = 0) {
+  if (l1.value === null || l2.value === null) return l3;
+  if (l3.value === null) {
+    l3.value === l1.value;
+    s = 1;
+  }
+  s = 0 ? zip(l1.next, l2, l3.next(new Node(l1.value)), s = 1) : zip(l1, l2.next, l3.next(new Node(l2.value)), s = 0);
 };
 
-module.exports = {Node: Node, zip: zip};
+module.exports = { Node: Node, zip: zip };
