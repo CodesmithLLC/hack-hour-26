99fe3f6259b56e117f33aff376551372952fa99a
joel burton <joeltburton@gmail.com>
2019-01-11 10:15:03 -0800

    i *think* its working

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,70 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+  // edge cases?
+  if (l1.val === null || l2.val === null) {
+    return l1.val !== null ? l1 : l2;
+  }
 
-module.exports = {Node: Node, zip: zip};
+  // grab nodes:
+  //  insertBehind (from l1)
+  //  insertBefore (from l1)
+  //  currentInsert (from l2)
+  //  nextInsert (from l2);
+
+  // check to see if any are tails -> handle tails
+
+  // else...
+  // insertBehind.next -> currentInsert.next -> insertBefore
+
+  // update working nodes
+  //  insertBehind = insertBehind.next.next
+  //  insertBefore = insertBefore.next
+  //  currentInsert = nextInsert
+  //  nextInsert = nextInsert.next
+
+  // repeat...
+
+  let insertBehind = l1;
+  let insertBefore = insertBehind.next;
+  let currentInsert = l2;
+  let nextInsert = currentInsert.next;
+
+
+  while (insertBefore !== null && currentInsert !== null) {
+    // zip
+    insertBehind.next = currentInsert;
+    currentInsert.next = insertBefore;
+
+    // then move down the list
+    insertBehind = insertBehind.next.next;
+    insertBefore = insertBefore.next;
+    currentInsert = nextInsert;
+    if (nextInsert.next !== null) nextInsert = nextInsert.next;
+  }
+
+  // found l1's tail
+  if (insertBefore === null) {
+    insertBehind.next = currentInsert;
+  }
+
+  // currentInsert.val === null -> found l2's tail;
+  return l1;
+}
+
+module.exports = { Node, zip };
+
+const odds = new Node(1);
+const evens = new Node(2);
+
+// odds.next = new Node(3);
+// odds.next.next = new Node(5);
+// odds.next.next.next = new Node(7);
+// odds.next.next.next.next = new Node(9);
+
+evens.next = new Node(4);
+// evens.next.next = new Node(6);
+// evens.next.next.next = new Node(8);
+// evens.next.next.next.next = new Node(10);
+
+console.log(zip(odds, evens));
