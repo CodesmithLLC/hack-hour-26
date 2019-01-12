166f7cfb56750ce3c61f17c52ef714d334be21a4
Chang Yea Moon <changyeamoon@gmail.com>
2019-01-12 09:50:26 -0800

    completed stack / no bonues

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,40 @@
  */
 
 function Stack() {
-  // body...
+  this.stack = [];
 }
+Stack.prototype.push = function a(val) {
+  this.stack[this.stack.length] = val;
+  return this.stack.length;
+};
+
+Stack.prototype.pop = function b() {
+  const popped = this.stack.splice(this.stack.length - 1, 1);
+  return popped;
+};
+
+
+Stack.prototype.getMax = function c() {
+  const temp = this.stack.slice();
+  temp.sort();
+  if (temp.length === 0) return -1;
+  return temp[temp.length - 1];
+};
+
 
 module.exports = Stack;
+
+const fake = new Stack();
+
+
+console.log(`Max: ${fake.getMax()}`);
+console.log(`Push (return length): ${fake.push(2)}`);
+console.log(`Push (return length): ${fake.push(3)}`);
+console.log(`Push (return length): ${fake.push(0)}`);
+console.log(fake.stack);
+console.log(`POP (return popped): ${fake.pop()}`);
+console.log(`Max: ${fake.getMax()}`);
+console.log(fake.stack);
+console.log(`POP (return popped): ${fake.pop()}`);
+console.log(`Max: ${fake.getMax()}`);
+console.log(fake.stack);
