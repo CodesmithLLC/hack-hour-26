cbe6d040ce9563581e08c1f61063f31d3c0a0b36
trischoe <tristans7@gmail.com>
2019-01-12 09:54:17 -0800

    max-stack approach

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,41 @@
  */
 
 function Stack() {
-  // body...
+  this.storage = {};
+  this.index = 0;
+  this.max = 0
+}
+
+Stack.prototype.push = function(value) {
+  let top = this.index;
+  this.storage[top] = value;
+  this.index += 1;
+  if(value >= this.max){this.max = value;}
+  return this.index;
+
+
+};
+
+Stack.prototype.pop = function() {
+  if(this.index === 0){
+    return undefined;
+  }
+  this.index = this.index -1
+  let retval = this.storage[this.index];
+  delete this.storage[this.index];
+  if(retval === this.max){
+    this.max = this.storage[0];
+    for(let i = 1; i < this.index; i++){
+      if(this.storage[i] > this.max){this.max = this.storage[i]};
+    }
+  }
+  return retval;
+
+};
+
+Stack.prototype.getMax = function(){
+  return this.max;
+
 }
 
 module.exports = Stack;
