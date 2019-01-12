1d2d9d01c3ca85392e64a592091ff7c4b9555228
JayCogen44 <jaycog44@gmail.com>
2019-01-12 09:52:42 -0800

    finished max-stack I think but didn't get the bonus yet..

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,39 @@
 
 function Stack() {
   // body...
+  this.storage = [];
+  this.count = 0;
+  this.max;
 }
+
+Stack.prototype.push = function (val) {
+  this.storage[this.count] = val;
+  this.count += 1;
+  return this.storage.length;
+}
+
+Stack.prototype.pop = function () {
+  this.count -= 1;
+  const del = this.storage[this.count];
+  this.storage.length = this.count;
+  return del;
+}
+
+Stack.prototype.getMax = function () {
+  this.storage.sort((a, b) => a < b);
+  return this.storage[0];
+}
+
+const stack = new Stack();
+
+stack.push(7);
+stack.push(12);
+stack.push(2);
+stack.push(11);
+
+console.log(stack.storage);
+console.log(stack.pop());
+console.log(stack.storage);
+console.log(stack.getMax());
 
 module.exports = Stack;
