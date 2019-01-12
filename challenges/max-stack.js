36f8238ff40bee9dbe4ae55a7b872fc0f4209e29
jordanzobean <jordanbetzer@gmail.com>
2019-01-12 09:55:44 -0800

    Finished max-stack and subset-sum

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,35 @@
  */
 
 function Stack() {
-  // body...
+  this.stack = [];
+  this.length = 0;
 }
+
+Stack.prototype.push = function(val) {
+  this.stack.push(val);
+  this.length++;
+  return this.length;
+};
+
+Stack.prototype.pop = function() {
+  const poppedElement = this.stack.pop();
+  this.length--;
+  return poppedElement;
+};
+
+Stack.prototype.getMax = function() {
+  const copiedStack = this.stack.slice();
+  const sortedCopy = copiedStack.sort((a, b) => b - a);
+  return sortedCopy[0];
+};
+
+const testStack = new Stack();
+testStack.push(1);
+testStack.push(3);
+testStack.push(2);
+testStack.push(10);
+console.log(testStack);
+
+console.log(testStack.getMax());
 
 module.exports = Stack;
