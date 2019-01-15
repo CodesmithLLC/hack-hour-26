856eb877276b5c8636c52f4c307ac9df56e44d8d
Byron Inocencio <jay.byron@gmail.com>
2019-01-15 09:29:27 -0800

    finished the challenge in place

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -13,8 +13,35 @@ function Node(value) {
     this.next = null;
 }
 
+let prevNode = null;
+let currNext = null;
+
 function reverseLinkedList(head) {
 
+
+    
+    if(head.next === null){
+        head.next = prevNode;
+        return head;
+    }
+
+    // STEP 1: save the curent next to keep going
+    currNext = head.next;
+
+    // STEP 2: set the current head to the previous node
+    head.next = prevNode;
+
+    // STEP 3: set prevNode equal to current
+
+    prevNode = head;
+
+
+    // STEP 4: move forard in the linked list
+    return reverseLinkedList(currNext);
+
+
+
 }
+
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
