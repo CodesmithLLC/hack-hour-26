49c18dfeec8bccbb7b001119c0de48066ac064e1
Chang Yea Moon <changyeamoon@gmail.com>
2019-01-15 09:31:25 -0800

    linked list reversed

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -9,12 +9,32 @@
  */
 
 function Node(value) {
-    this.value = value;
-    this.next = null;
+  this.value = value;
+  this.next = null;
 }
 
 function reverseLinkedList(head) {
-
+  let p = head;
+  let c = head.next;
+  if (head.next === null) return head;
+  head.next = null;
+  while (c !== null) {
+    p = head;
+    head = c;
+    c = head.next;
+    head.next = p;
+  }
+  return head;
 }
 
-module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
+module.exports = { Node, reverseLinkedList };
+
+// const one = new Node(1);
+// const two = new Node(2);
+// const three = new Node(3);
+
+
+// one.next = two;
+// two.next = three;
+
+// console.log(reverseLinkedList(one));
