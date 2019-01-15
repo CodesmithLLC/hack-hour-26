ff95d41a5b62b46b918fbd079b80422d52bb8f89
jordanzobean <jordanbetzer@gmail.com>
2019-01-15 09:53:11 -0800

    finished reverse-linked-list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -9,12 +9,39 @@
  */
 
 function Node(value) {
-    this.value = value;
-    this.next = null;
+  this.value = value;
+  this.next = null;
 }
 
 function reverseLinkedList(head) {
+  // check if head.next is null
+  if (head.next === null) return head;
+  // create three pointers, p1, p2 and p3
+  let p1 = head;
+  let p2 = head.next;
+  let p3 = p2.next;
 
+  while (true) {
+    if (p2.next === null) {
+      p2.next = p1;
+      break;
+    }
+    p2.next = p1;
+    p1 = p2;
+    p2 = p3;
+    p3 = p3.next;
+  }
+  //point head to null
+  head.next = null;
+  return p2;
 }
 
-module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+a.next = b;
+b.next = c;
+
+console.log(reverseLinkedList(a));
+
+module.exports = { Node: Node, reverseLinkedList: reverseLinkedList };
