2e084af62cd68a7e78b240557aea36c7daf1f110
Harmon Huynh <harmon.huynh@outlook.com>
2019-01-17 09:31:29 -0800

    Completed merge array

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -14,7 +14,10 @@
  */
 
 function mergeArrays(arr1, arr2) {
-
+  const newArray = arr1.concat(arr2);
+  return newArray.sort((a, b) => {
+    return a - b;
+  })
 }
 
 module.exports = mergeArrays;
