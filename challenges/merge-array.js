cbc6b1e0ebf88cbd215629481ac23e5443a74236
howiekick <howardna317@gmail.com>
2019-01-17 09:31:48 -0800

    finished merge arrays, will attempt O(n) next

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -14,7 +14,17 @@
  */
 
 function mergeArrays(arr1, arr2) {
+    const concatArrs = arr1.concat(arr2)
 
+    return concatArrs.sort((a, b) => {
+        return a - b;
+    })
 }
+
+
+// var my_array = [3,4,6,10,11,15,21];
+// var another_array = [1,5,8,12,14,19];
+
+// console.log(mergeArrays(my_array, another_array))
 
 module.exports = mergeArrays;
