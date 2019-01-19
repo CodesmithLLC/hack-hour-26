fc2648c74c2c23421069eebd29ab87dff3a2a768
Byron Inocencio <jay.byron@gmail.com>
2019-01-19 09:35:48 -0800

    fixed perm palindrome

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -14,6 +14,69 @@
 
 function numToWords(num) {
 
+  const wordTable = {
+    0: 'Zero',
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+    10: 'Ten',
+    11: 'Eleven',
+    12: 'Twelve',
+    13: 'Thirteen',
+    14: 'Fourtneen',
+    15: 'Fifteen',
+    16: 'Sixteen',
+    17: 'Seventeen',
+    18: 'Eighteen',
+    19: 'Nineteen'
+  }
+
+  const powerTable = {
+    20: 'Twenty',
+    30: 'Thirty',
+    40: 'Forty',
+    50: 'Fifty',
+    60: 'Sixty',
+    70: 'Seventy',
+    80: 'Eighty',
+    90: 'Ninety',  
+    100: 'Hundred',
+    1000: 'Thousand',
+    1000000: 'Million',
+    1000000000: 'Billion',
+    1000000000000: 'Quadrillion'
+  }
+
+  let base = Object.keys(powerTable);
+  let baseKeys = Object.values(powerTable);
+  base = base.map((x) => parseInt(x,10))
+
+
+  console.log('this is our num:', num)
+
+  if(wordTable[num]){
+    return wordTable[num];
+  }
+
+  else{
+    for (let i =0; i<base.length; i++){
+      if(num > base[i]){
+        console.log(baseKeys[i]);
+        let stripped = num/base[i]*10;
+        console.log('stripped:',stripped)
+        break;
+      }
+    }
+  }
+
 }
+
+numToWords(210);
 
 module.exports = numToWords;
--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,39 @@
  */
 
 function permPalin(str) {
-	
+  const charCount = {};
+  
+  let charArray = str.split('');
+
+
+  for(let i =0; i<charArray.length; i++){
+    if(charCount[charArray[i]]) {
+      charCount[charArray[i]] += 1;
+    }
+
+    else{
+      charCount[charArray[i]] = 1;
+    }
+
+  }
+
+  const entries = Object.entries(charCount);
+
+  let pivot = 0;
+
+  for(let j=0; j<entries.length; j++ ){
+    console.log(entries[j][1])
+    if(entries[j][1] % 2){
+      pivot += 1;
+    }
+  }
+
+  if(pivot>1){return false}
+
+  return true;
+
+
 }
+
 
 module.exports = permPalin;
