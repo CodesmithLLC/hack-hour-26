e57d947991e22329d8046cceffa99e4550305173
trischoe <tristans7@gmail.com>
2019-01-19 09:31:34 -0800

    permPalin approach

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,6 +10,19 @@
  */
 
 function permPalin(str) {
+    let oddcount = 0;
+    const strObj = {};
+    for(let i= 0; i < str.length; i++){
+      strObj[str[i]] = (!strObj[str[i]]) ? 1 : strObj[str[i]] +=1;
+    }
+    let countArr = Object.values(strObj);
+    for(let i = 0; i < countArr.length; i++){
+        if(countArr[i] % 2 !== 0){
+            oddcount += 1;
+        }
+    }
+    return (oddcount > 1) ? false : true;
+
 	
 }
 
