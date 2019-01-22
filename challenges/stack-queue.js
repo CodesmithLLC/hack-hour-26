347cfaf07bedc5b146d9a2bd2a9ccc306f955b17
Chang Yea Moon <changyeamoon@gmail.com>
2019-01-22 10:08:13 -0800

    completed stack-queueuueueueueueueueueueue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,8 +4,22 @@
 
 
 function Stack() {
-
+  this.stack = {};
+  this.index = 0;
 }
+
+Stack.prototype.push = function b(val) {
+  this.stack[this.index] = val;
+  this.index += 1;
+  return this.index;
+};
+
+Stack.prototype.pop = function a() {
+  this.index -= 1;
+  const temp = this.stack[this.index];
+  delete this.stack[this.index];
+  return temp;
+};
 
 
 /**
@@ -14,7 +28,46 @@ function Stack() {
 
 
 function Queue() {
-
+  this.queue = new Stack();
+  this.queue2 = new Stack();
+  this.index = 0;
 }
 
-module.exports = {Stack: Stack, Queue: Queue};
+Queue.prototype.enqueue = function c(val) {
+  this.queue.push(val);
+  return this.queue.index;
+};
+
+Queue.prototype.dequeue = function d() {
+  while (this.queue.index > 0) {
+    this.queue2.push(this.queue.pop());
+  }
+  const temp = this.queue2.pop();
+  while (this.queue2.index > 0) {
+    this.queue.push(this.queue2.pop());
+  }
+  return temp;
+};
+
+module.exports = { Stack, Queue };
+
+
+// const stack1 = new Stack();
+// const stack2 = new Stack();
+// const queue = new Queue();
+
+// console.log('------ stack ------');
+// console.log(stack1.push(1));
+// console.log(stack1.push(2));
+// console.log(stack1.push(3));
+// console.log(stack1.stack);
+// console.log(stack1.pop());
+// console.log(stack1.stack);
+
+
+// console.log('------ queue ------');
+// console.log(queue.enqueue(1));
+// console.log(queue.enqueue(2));
+// console.log(queue.queue.stack);
+// console.log(queue.dequeue());
+// console.log(queue.queue.stack);
