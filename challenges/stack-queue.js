f2dfc8bb9655d7368f8260f71762d7b9f70c7e11
JayCogen44 <jaycog44@gmail.com>
2019-01-22 10:22:57 -0800

    finished stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,9 +4,18 @@
 
 
 function Stack() {
-
+    this.content = [];
+    this.length = 0;
 }
 
+Stack.prototype.push = function (x) {
+    this.content.push(x);
+    this.length += 1;
+}
+Stack.prototype.pop = function () {
+    this.length -= 1;
+    return this.content.pop();
+}
 
 /**
 * Queue Class
@@ -14,7 +23,23 @@ function Stack() {
 
 
 function Queue() {
-
+    this.stackA = new Stack();
+    this.stackB = new Stack();
 }
 
-module.exports = {Stack: Stack, Queue: Queue};
+Queue.prototype.enqueue = function (x) {
+    const elements = this.stackA.length;
+    for (let i = 0; i < elements; i++) {
+        this.stackB.push(this.stackA.pop());
+    }
+    this.stackA.push(x);
+    for (let i = 0; i < elements; i++) {
+        this.stackA.push(this.stackB.pop());
+    }
+}
+
+Queue.prototype.dequeue = function () {
+    return this.stackA.pop();
+}
+
+module.exports = { Stack: Stack, Queue: Queue };
