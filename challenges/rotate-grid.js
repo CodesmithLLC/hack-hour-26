04b416ea4c6af8f36c369815ba8255db80580dda
Byron Inocencio <jay.byron@gmail.com>
2019-01-22 10:01:29 -0800

    finished stack-queue

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -12,12 +12,40 @@
  *                  sampleGrid after:   [   [7, 4, 1],
  *                                          [8, 5, 2],
  *                                          [9, 6, 3]  ]
+ * 
+ * [1 ,2]
+ * [3, 4]
+ * 
+ * [3,1]
+ * [2,4]
  *
  * BONUS: Do this in place
+ * 
+ *
+ *}
+ * 
  */
 
 function rotateGrid(grid, n) {
 
+  //loop through the outer matrix
+  for(let i =0; i<n; i++){
+    for(let j=0; j<i; j++){
+
+      
+
+    }
+ 
+  }
+
+  return grid;
+
 }
+
+
+let a =  [   [1, 2, 3],[4, 5, 6],[7, 8, 9]  ]
+
+console.log(a);
+console.log(rotateGrid(a,3))
 
 module.exports = rotateGrid;
