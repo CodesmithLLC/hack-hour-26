c7a9984e5f7a9e24b7ac5451ab43d95f9fb1786f
Carolyn Harrold <carolyn.harrold@gmail.com>
2019-01-23 09:41:06 -0800

    two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,15 @@
  */
 
 function twoSum(arr, n) {
-
+  //loop through the array 
+  for (let i = 0; i < arr.length; i++) {
+    //in a second loop start at j= i+1, and loop through testing i + j, if that equals the sum, return true
+    for (let j = i + 1; j < arr.length; j++) {
+      if ((arr[i] + arr[j]) === n) return true;
+    }
+  }      
+  //if we exit the loops, return false
+  return false;
 }
 
 module.exports = twoSum;
