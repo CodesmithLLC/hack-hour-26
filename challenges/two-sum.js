03b017a67cfc320425438a1840f959ebb38c60de
emiliacarmel <emiliacarmel@gmail.com>
2019-01-23 09:25:43 -0800

    two-sum hack hour attempt

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,25 @@
  */
 
 function twoSum(arr, n) {
-
+  // iterate through array
+  const pairingArray = []
+  for (let i = 0; i < arr.length; i += 1) {
+    // check the pairing array at each iteration in case array[i] is a match there
+    if (pairingArray.includes(arr[i])) {
+      return true;
+    } else {
+      // calculate the opposing number to be equal to target, then push that number into pairing array
+      pairingArray.push(n-arr[i]);
+    }
+  }
+  // otherwise return false
+  return false;
 }
+
+
+const nums = [3, 1, 5, 8, 3, 9];
+console.log(twoSum(nums, 14));
+console.log(twoSum(nums, 15));
+
 
 module.exports = twoSum;
