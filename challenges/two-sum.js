a51aee23fdd8b38ede2034fb3d3d4869f697eefe
Byron Inocencio <jay.byron@gmail.com>
2019-01-23 09:38:29 -0800

    finished two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -4,6 +4,33 @@
 
 function twoSum(arr, n) {
 
+
+  const currentVals = {};
+
+  //loop through all the input array elements
+  for(let i =0; i<arr.length; i++){
+
+    //calculate the value we need to complete the sum
+    let sub = n - arr[i]
+
+
+    //check if we've already seen that value
+    if((currentVals[sub.toString()])){
+
+      //if we found it, we found two sums that equal the number! return true
+      return true;
+      
+    }
+
+    //add the current value to our list of values we've seen
+    currentVals[arr[i].toString()] = arr[i];
+  }
+
+
+  return false;
+
 }
 
+
+// console.log(twoSum([4, 5, 1, 8],6))
 module.exports = twoSum;
