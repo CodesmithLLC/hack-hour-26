849e2846e28f647112bce56ed490d5282cff23a8
bradenaa <bradenaltstatt@gmail.com>
2019-01-23 09:38:48 -0800

    checking tests for two-sum hack-hour

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -2,8 +2,10 @@
  *  array that sum up to the target value; return false otherwise
  */
 
-function twoSum(arr, n) {
-
+function twoSum(arr, n, numbers = 0) {
+  if (n === 0 && numbers === 2) return true;
+  if (arr.length === 0) return false;
+  return twoSum(arr.slice(1), n, numbers) || twoSum(arr.slice(1), n - arr[0], numbers + 1);
 }
 
 module.exports = twoSum;
