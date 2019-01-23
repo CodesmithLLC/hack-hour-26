be6cd9ff51784ce8deb20a94b086b7844c958c03
Adrian Diamond <adriandiamond@Adrians-MacBook-Air.local>
2019-01-23 10:01:12 -0800

    completed two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,24 @@
  */
 
 function twoSum(arr, n) {
-
+    //loop over the array and subtract each value from the target number.
+    let newObj = {}; 
+    for(let i=0; i<arr.length; i++){
+        //store each value as a key in an object and the difference as the value.
+        newObj[arr[i]] = (n - arr[i])
+    }
+    //loop over the object, check to see that the difference exists in the object
+    for(prop in newObj){
+        if((newObj[prop] != prop) && (arr.includes(newObj[prop]))){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
+
+// let testArr = [1,3,7]
+
+// console.log(twoSum(testArr, 2))
 
 module.exports = twoSum;
