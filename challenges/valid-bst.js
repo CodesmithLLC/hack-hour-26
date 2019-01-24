c660171d1624f7639181c42861816712183c26de
JayCogen44 <jaycog44@gmail.com>
2019-01-24 09:59:18 -0800

    valid bst

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -4,7 +4,7 @@
  *      at any given node, the value of all the nodes in its right tree must be > its value
  * Assume that each value in the tree is unique.
  */
- 
+
 
 function BinaryTree(val) {
     this.value = val;
@@ -12,8 +12,34 @@ function BinaryTree(val) {
     this.right = null;
 }
 
-function validBST(tree) {
+// BinaryTree.prototype.add = function (value) {
+//     if (value !== this.value) {
+//         if (value > this.value && this.right !== null) return this.right.add(value);
+//         if (value < this.value && this.left !== null) return this.left.add(value);
+//         if (value > this.value) this.right = new BinaryTree(value);
+//         if (value < this.value) this.left = new BinaryTree(value);
+//     }
+// };
 
+// const bst = new BinaryTree(10);
+
+// bst.add(8);
+// bst.add(2);
+// bst.add(4);
+// bst.add(16);
+// bst.add(12);
+
+
+function validBST(tree) {
+    if (tree.left === null) return true;
+    if (tree.right === null) return true;
+    if (tree.value < tree.right.value) return validBST(tree.right);
+    if (tree.value > tree.left.value) return validBST(tree.left);
+    return false;
 }
 
-module.exports = {BinaryTree: BinaryTree, validBST: validBST};
+// console.log(bst);
+// console.log(validBST(bst));
+
+
+module.exports = { BinaryTree: BinaryTree, validBST: validBST };
