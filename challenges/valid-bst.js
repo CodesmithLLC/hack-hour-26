fd672c33dcb40e4ed3cf21b0798bd6518dfe7e7c
Byron Inocencio <jay.byron@gmail.com>
2019-01-24 09:51:39 -0800

    completed valid bst

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -14,6 +14,55 @@ function BinaryTree(val) {
 
 function validBST(tree) {
 
+    let leftValidation, rightValidation;
+
+    //check left
+    if(tree.left) {
+        // console.log('checkeing left: ',tree.left.value, tree.value)
+        if (tree.left.value > tree.value) {
+            // console.log('should be false')
+            return false;     
+        } else {
+            leftValidation = validBST(tree.left);
+        }
+    }
+
+    // check right
+    if(tree.right) {
+
+        // console.log('checkeing right: ',tree.right.value, tree.value)
+
+        if (tree.right.value < tree.value) {
+            // console.log('should be false')
+            return false;
+        } else {
+             rightValidation = validBST(tree.right);
+        }
+    }
+
+    if(leftValidation === false || rightValidation ===false){
+        return false;
+    }
+
+    return true;
 }
+
+// let a = new BinaryTree(5);
+// let b = new BinaryTree(3);
+// let c = new BinaryTree(8);
+// let d = new BinaryTree(2);
+// let e = new BinaryTree(4);
+// let f = new BinaryTree(63442);
+// let g = new  BinaryTree(8);
+
+// a.left = b;
+// b.left = d;
+// b.right = e;
+// a.right = c;
+// c.left = f;
+// c.right = g;
+
+
+// console.log(validBST(a));
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
