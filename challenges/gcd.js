65c8fb8495a2fbe5d7b9792d3d27412b1644335f
Chang Yea Moon <changyeamoon@gmail.com>
2019-01-25 09:39:16 -0800

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,20 @@
  */
 
 function gcd(a, b) {
-
+  const start = (a < b) ? Math.floor(a / 2) : Math.floor(b / 2);
+  //   console.log(start);
+  for (let i = start; i > 0; i -= 1) {
+    // console.log(a % i);
+    if (a % i === 0 && b % i === 0) {
+      return i;
+    }
+  }
 }
 
+
 module.exports = gcd;
+
+
+console.log(gcd(10, 8));
+console.log(gcd(10, 9));
+
