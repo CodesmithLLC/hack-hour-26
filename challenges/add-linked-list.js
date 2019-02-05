fe7db44d2db7326a8bdec64b3c927fd4d0d9db9e
howiekick <howardna317@gmail.com>
2019-02-05 09:38:58 -0800

    finished add-linked-list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -18,7 +18,43 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  let firstArr = [];
+  let node = l1;
 
+  while(node) {
+    firstArr.push(node.value)
+    node = node.next;
+  }
+
+  let secondArr = [];
+  let node2 = l2;
+
+  while(node2) {
+    secondArr.push(node2.value);
+    node2 = node2.next;
+  }
+
+  let firstNum = parseInt(firstArr.reverse().join(''));
+  let secondNum = parseInt(secondArr.reverse().join(''));
+
+  return firstNum + secondNum
 }
+
+// const link1a = new Node(3)
+// const link1b = new Node(2)
+// const link1c = new Node(1)
+
+// link1a.next = link1b
+// link1b.next = link1c
+
+// const link2a = new Node(6)
+// const link2b = new Node(5)
+// const link2c = new Node(4)
+
+// link2a.next = link2b
+// link2b.next = link2c
+// console.log(link1a)
+
+// console.log(addLinkedList(link1a, link2a))
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
