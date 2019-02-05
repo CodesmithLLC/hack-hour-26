4d50a1b0237ccedc3bfc2c45777abeecbb26a3d7
Carolyn Harrold <carolyn.harrold@gmail.com>
2019-02-05 09:29:06 -0800

    count-twos

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -10,7 +10,31 @@
 
 
 function countTwos(num) {
-
+  //declare a variable twoCount and set equal to 0
+  let twoCount = 0;
+  let numString = '';
+  //initiate a for loop starting at 0 and increasing until num + 1
+  for (let i = 1; i < num + 1; i++) {
+    //convert the number to a string
+    numString = i.toString();
+    //loop through the string 
+    for (let j = 0; j < numString + 1; j++) {
+      //see if there are any twos, if two
+      if (numString[j] === '2') {
+      //increment twoCount
+      twoCount += 1;
+    }
+    }
+    }
+  //return twoCount
+  return twoCount;
 }
+
+console.log(countTwos(1)) //;  -> 0
+console.log(countTwos(3)) //;  -> 1
+console.log(countTwos(13)) //;  -> 2
+console.log(countTwos(1000)) //;  -> 300
+console.log(countTwos(11420)) //; -> 4483
+
 
 module.exports = countTwos;
