52f1a2cd99be16d21bd67ae59063f99ee147e76f
bradenaa <bradenaltstatt@gmail.com>
2019-02-06 09:45:15 -0800

    completed the superbalanced hack-hour for BSTs

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -14,7 +14,50 @@ function BinaryTree(value) {
 }
 
 function superbalanced(tree) {
+ // tree left and tree right are null, then return true
+ if (tree.left === null && tree.right === null) return true;
+ let depthLeft = 0;
+ let depthRight = 0;
 
+ // check if the left tree is balanced and count the depth
+// if it isn't balanced then return false
+ function recurseLeftBST(bst, min, max) {
+   // if reached the end, we want to return true
+   if (!bst) return true;
+   depthLeft += 1;
+   // if current value is greater then the max return false
+   if (bst.value >= max) return false;
+   // if the current value is greater then the min, return false
+   if (bst.value <= min) return false;
+   // recurse the left side, where max is the current value and min stays the same
+   // recurse the right side, where max stays the same and min is the current value
+   return recurseLeftBST(bst.left, min, bst.value) && recurseLeftBST(bst.right, bst.value, max);
+ };
+  recurseLeftBST(tree.left, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
+
+
+  // check if the right tree is balanced and count the depth
+  // if it isn't balanced then return false
+  function recurseRightBST(bst, min, max) {
+    // if reached the end, we want to return true
+    if (!bst) return true;
+    depthRight += 1;
+    // if current value is greater then the max return false
+    if (bst.value >= max) return false;
+    // if the current value is greater then the min, return false
+    if (bst.value <= min) return false;
+    // recurse the left side, where max is the current value and min stays the same
+    // recurse the right side, where max stays the same and min is the current value
+    return recurseRightBST(bst.left, min, bst.value) && recurseRightBST(bst.right, bst.value, max);
+  };
+  recurseRightBST(tree.right, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
+
+ // if the difference between the left depth and the right depth is greater then 1, return false
+ const difference = Math.abs(depthLeft - depthRight);
+ if (difference > 1) return false;
+ // otherwise, return true
+ return true;
 }
+
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
