7fcbd6f38ad9920246e41412b10a31f114ae57d7
Carolyn Harrold <carolyn.harrold@gmail.com>
2019-02-07 09:59:17 -0800

    repeat-number

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,25 @@
  */
 
 function repeatNumbers(array) {
-
+  let storage = {};
+  for (let i = 0; i < array.length; i ++) {
+    //check our storage object for array[i];
+    if (storage[array[i]]) {
+    //if there, return that element
+    storage = null;
+    return array[i];
+  }
+    //if not there, add to storage object
+    else {
+      storage[array[i]] = true;
+    }
+  }
 }
+
+let array = [1, 5, 6, 7, 8, 5];
+console.log(repeatNumbers(array))
+
+
+
 
 module.exports = repeatNumbers;
