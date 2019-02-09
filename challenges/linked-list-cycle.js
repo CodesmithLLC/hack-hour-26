e9f71ce5584d4f364ba34117125ae9eadec33cbe
joel burton <joeltburton@gmail.com>
2019-02-09 09:35:40 -0800

    completed linked-list-cycle. I *think* its constant space, since all the pointers are just references to objects already created in memory

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -27,13 +27,38 @@
  *
  */
 
-var Node = function(value) {
+const Node = function (value) {
   this.value = value;
   this.next = null;
-}
+};
 
 function hasCycle(head) {
+  const nodes = new Set();
+  let current = head;
 
+  while (!nodes.has(current)) {
+    // found the tail -> no cycles -> return false
+    if (current.next === null) return false;
+
+    // not at the tail yet, keep adding to nodes and traverse
+    nodes.add(current);
+    current = current.next;
+  }
+
+  // broke out of loop -> nodes has a reference we've already visited
+  return true;
 }
 
-module.exports = {Node: Node, hasCycle: hasCycle}
+
+const node1 = new Node('1');
+const node2 = node1.next = new Node('2');
+const node3 = node2.next = new Node('3');
+const node4 = node3.next = new Node('4');
+const node5 = node4.next = new Node('5');
+
+console.log(`false: ${hasCycle(node1)}`);
+
+node5.next = node2;
+console.log(`true: ${hasCycle(node1)}`);
+
+module.exports = { Node, hasCycle };
