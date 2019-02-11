7569c1d8ec2461a53b338fd5cb3865a3646c8fc1
Adrian Diamond <adriandiamond@Adrians-MacBook-Air.local>
2019-02-11 09:31:50 -0800

    Completed multiples of 3 or 5 challenge

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -5,8 +5,37 @@
 // write a function that will find the sum of all the multiples of 3 or 5
 // below 1000 and return that sum.
 
+function sumMultiples3Or5Below10() {
+  let sum = 0;
+  let counter = 1;
+  let count = 1;
+  while (3*counter < 10){
+      sum+=(3*counter);
+      counter++;
+  }
+ 
+  while (5*count < 10){
+    sum+=(5*count);
+    count++;
+  }
+
+  return sum;
+}
+
+
 function sumMultiples3Or5Below1000() {
   let sum = 0;
+  let counter = 1;
+  let count = 1;
+  while (3*counter < 1000){
+      sum+=(3*counter);
+      counter++;
+  }
+ 
+  while (5*count < 1000){
+    sum+=(5*count);
+    count++;
+  }
 
   return sum;
 }
@@ -14,11 +43,24 @@ function sumMultiples3Or5Below1000() {
 
 // extension make it dynamic function that takes input x,y,z
 // and returns the sum of multiples of x and y below z
-function sumMultiplesXOrYBelowZ(x, y, z) {
+function sumMultiplesXOrYBelowZ(x,y,z) {
   let sum = 0;
+  let counter = 1;
+  let count = 1;
+  while (x*counter < z){
+      sum+=(x*counter);
+      counter++;
+  }
+ 
+  while (y*count < z){
+    sum+=(y*count);
+    count++;
+  }
 
   return sum;
 }
+
+
 
 const objectToExport = {
   sumMultiples3Or5Below1000,
@@ -26,3 +68,5 @@ const objectToExport = {
 };
 
 module.exports = objectToExport;
+
+
