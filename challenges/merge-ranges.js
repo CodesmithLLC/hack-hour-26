25b328c35fa3358acb0dc7a695b3a65a552e39c6
howiekick <howardna317@gmail.com>
2019-02-11 09:41:54 -0800

    finished multiples of 3 or 5

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -1,5 +1,6 @@
 /**
- * Write a function mergeRanges that takes an array of meeting time ranges and returns an array of condensed ranges.
+ * Write a function mergeRanges that takes an array of meeting time ranges 
+ * //and returns an array of condensed ranges.
  *
  * Example:
  * var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
@@ -12,6 +13,17 @@
 
 function mergeRanges(array) {
 
+    let times = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12]]
+
+    // push true, or mark time ranges
+    
 }
+var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
+
+
+console.log(concatEm(times))
+
+
+
 
 module.exports = mergeRanges;
