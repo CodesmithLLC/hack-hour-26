842f138ee47d6daf59c7d72bcd89b9b548ddaaea
JayCogen44 <jaycog44@gmail.com>
2019-02-15 09:08:25 -0800

    started on delete dupes but didn't finish

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,55 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
+function LinkedList() {
+    this.head = null;
+    this.tail = null;
+}
 
+function Node(val) {
+    this.value = val;
+    this.next = null;
+}
+
+LinkedList.prototype.push = function (val) {
+    const newNode = new Node(val)
+    if (this.head === null) this.head = newNode;
+    if (this.tail === null) {
+        this.tail = newNode;
+    } else {
+        this.tail.next = newNode;
+        this.tail = newNode
+    }
+}
+
+const ll = new LinkedList();
+ll.push(8);
+ll.push(3);
+ll.push(5);
+ll.push(3);
+ll.push(7);
+ll.push(12);
+ll.push(10);
+ll.push(1);
 
 function deleteDups(head) {
-
+    let curNode = head;
+    let buffer = new Set([curNode.value]);
+    while (curNode.next !== null) {
+        console.log(curNode.value);
+        if (buffer.has(curNode.value)) {
+            curNode.value = curNode.next.value;
+        } else {
+            buffer.add(curNode.next.value);
+        }
+        curNode = curNode.next;
+    }
+    console.log(buffer);
+    return head;
 }
+
+deleteDups(ll.head);
+
+console.log(JSON.stringify(ll, null, 4));
 
 module.exports = deleteDups;
