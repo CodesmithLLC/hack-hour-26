ed1fc123d67de73c4f13721ad52fefab7eb7d559
JayCogen44 <jaycog44@gmail.com>
2019-02-16 09:29:19 -0800

    found the logorithmic solution to find in ordered set

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,7 +11,12 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
-
+    while (arr.length >= 1) {
+        const middle = Math.floor(arr.length / 2);
+        if (target === arr[middle]) { return true; }
+        else if (arr.length === 1) { return false; }
+        target > arr[middle] ? arr.splice(0, middle) : arr.splice(middle, (arr.length - middle));
+    }
 }
 
 
