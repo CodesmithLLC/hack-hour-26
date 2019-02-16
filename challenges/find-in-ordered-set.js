b76528d397adf73906a82c2a2ea51f9f68ed86d3
JayCogen44 <jaycog44@gmail.com>
2019-02-16 09:51:32 -0800

    did it recursively...

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,13 +11,30 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
-    while (arr.length >= 1) {
-        const middle = Math.floor(arr.length / 2);
-        if (target === arr[middle]) { return true; }
-        else if (arr.length === 1) { return false; }
-        target > arr[middle] ? arr.splice(0, middle) : arr.splice(middle, (arr.length - middle));
+    if (!Array.isArray(arr)) return false;
+    function recHelper(recArr) {
+        const middle = Math.floor(recArr.length / 2);
+        if (target === recArr[middle]) { return true; }
+        else if (recArr.length === 1) { return false; }
+        return target > recArr[middle] ?
+            recHelper(recArr.splice(0, middle)) :
+            recHelper(recArr.splice(middle, (recArr.length - middle)));
     }
+    return recHelper(arr);
 }
+
+// function findInOrderedSet(arr, target) {
+//     while (arr.length >= 1) {
+//         const middle = Math.floor(arr.length / 2);
+//         if (target === arr[middle]) { return true; }
+//         else if (arr.length === 1) { return false; }
+//         target > arr[middle] ? arr.splice(0, middle) : arr.splice(middle, (arr.length - middle));
+//     }
+// }
+
+// var nums = [1, 4, 6, 7, 9, 17, 45];
+// console.log(findInOrderedSet(nums, 7));
+// console.log(nums);
 
 
 module.exports = findInOrderedSet;
