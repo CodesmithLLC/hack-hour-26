6e6e5d6e1aa132c2e80dfd2dafc54226e100e75e
Byron Inocencio <jay.byron@gmail.com>
2019-02-16 09:36:21 -0800

    finished find in order

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -12,7 +12,41 @@ findInOrderedSet(nums, 2);  -> false
 
 function findInOrderedSet(arr, target) {
 
+
+  let inner = (arr) => {
+
+    // console.log(arr);
+    if(arr.length <= 0){
+      return false;
+    }
+
+    let middle = Math.floor(arr.length/2);
+
+    // console.log('comparing:', arr[middle],target)
+    if(arr[middle] === target) {
+      return true;
+    }
+
+    if(arr[middle]> target) {
+      return findInOrderedSet(arr.slice(0,middle),target);
+    }
+
+    else {
+      if(arr.length===1){
+        return findInOrderedSet([],target)
+      } else{
+
+        return findInOrderedSet(arr.slice(middle),target);
+      }
+    }
+  }
+
+  let ret = inner(arr);
+  return ret;
+
 }
 
+// var nums = [1, 4, 6, 7, 9, 17, 45]
+// console.log(findInOrderedSet(nums,1));  
 
 module.exports = findInOrderedSet;
