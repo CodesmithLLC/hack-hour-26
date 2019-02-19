a04cd65632cfc38ac43232e53bd522fcf9708cc6
JayCogen44 <jaycog44@gmail.com>
2019-02-19 09:38:17 -0800

    insertion sort... naive?

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,19 @@
 
 
 function insertionSort(array) {
-
+    for (let i = 1; i < array.length; i++) {
+        let j = i;
+        while (j > 0 && array[j - 1] > array[j]) {
+            const curEl = array[j - 1];
+            array[j - 1] = array[j];
+            array[j] = curEl;
+            j -= 1;
+        }
+    }
+    return array;
 }
+
+// const a = [5, 8, 6, 12, 1, 3];
+// console.log(insertionSort(a));
 
 module.exports = insertionSort;
