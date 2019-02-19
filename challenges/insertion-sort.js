4447d48ba145b9d8eb91bac510cc404696a37af2
trischoe <tristans7@gmail.com>
2019-02-19 09:37:20 -0800

    insertion sort approach

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,16 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 0; i < array.length; i++) {
+    let j = i - 1;
+    let value = array[i];
+      for (j; j > - 1 && array[j] > value; j--) {
+        array[j + 1] = array[j];
+      }
+      array[j + 1] = value;
+  }
+  return array;
 }
+
 
 module.exports = insertionSort;
