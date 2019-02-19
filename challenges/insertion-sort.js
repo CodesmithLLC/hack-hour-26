08e7c7b36408e2699abb7fe49bcd9180ec1698e2
joel burton <joeltburton@gmail.com>
2019-02-19 09:44:49 -0800

    refactored to 0(1) space complexity

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -3,9 +3,46 @@
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 
+// function insertionSort(array) {
+//   if (array.length === 0) return [];
+
+//   const output = [array[0]];
+//   for (let i = 1; i < array.length; i += 1) {
+//     const element = array[i];
+//     let insertIndex = output.length;
+
+//     while (insertIndex > 0) {
+//       if (element < output[insertIndex - 1]) insertIndex -= 1;
+//     }
+
+//     output.splice(insertIndex, 0, element);
+//   }
+
+//   return output;
+// }
 
 function insertionSort(array) {
+  if (array.length === 0) return [];
 
+  for (let i = 0; i < array.length; i += 1) {
+    const element = array.splice(i, 1)[0];
+    let insertIndex = i;
+
+    while (insertIndex > 0) {
+      if (element < array[insertIndex - 1]) insertIndex -= 1;
+    }
+
+    array.splice(insertIndex, 0, element);
+  }
+
+  return array;
 }
+
+const unsorted = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+
+const sorted = insertionSort(unsorted);
+
+console.log(sorted);
+
 
 module.exports = insertionSort;
