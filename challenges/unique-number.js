96376c5b9837b4caec6a7d52b6c2172e0900f609
JayCogen44 <jaycog44@gmail.com>
2019-02-20 09:41:26 -0800

    O(n) time but O(n) space

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -2,7 +2,7 @@
  * Write a function that will take an array of integers, all of which will appear exactly twice,
  * except for one integer that will appear exactly once. Return the integer that appears once.
  *
- * uniqueNumber([1,2,1,3,3]); -> 2
+ * uniqueNumber([1,7,2,1,3,7,3]); -> 2
  *
  * BONUS:
  * Complete the challenge in O(n) time
@@ -10,7 +10,13 @@
  *
  */
 function uniqueNumber(array) {
-
+    const wrkObj = new Set();
+    array.forEach(e => {
+        wrkObj.has(e) ? wrkObj.delete(e) : wrkObj.add(e);
+    });
+    return wrkObj.values().next().value;
 }
+
+console.log(uniqueNumber([1, 7, 2, 1, 3, 7, 3]));
 
 module.exports = uniqueNumber;
