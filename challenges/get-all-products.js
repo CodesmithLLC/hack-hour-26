7fca4e2bb796716d8457b5ab012d1a02e27e2ccb
jordanzobean <jordanbetzer@gmail.com>
2019-02-21 09:26:05 -0800

    Finished get-all-products

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -1,4 +1,5 @@
-/* Accepts an array of integers and returns an array of all the possible products made by
+/*
+ * Accepts an array of integers and returns an array of all the possible products made by
  * multiplying all but one number. In other words, find all the products of multiplying any
  * array.length-1 numbers in the array.
  *
@@ -10,7 +11,26 @@
  */
 
 function getAllProducts(array) {
-
+  // * Create an output array
+  const output = [];
+  // * Loop over input array
+  for (let i = 0; i < array.length; i++) {
+    // * Declare product var
+    let product = 1;
+    // *Loop over array again
+    for (let j = 0; j < array.length; j++) {
+      // * Multiply all numbers in array together except current element in outer loop
+      if (array[i] !== array[j]) {
+        product *= array[j];
+      }
+    }
+    // * Push product into output array
+    output.push(product);
+  }
+  // * Return output
+  return output;
 }
+
+console.log(getAllProducts([1, 7, 3, 4]));
 
 module.exports = getAllProducts;
