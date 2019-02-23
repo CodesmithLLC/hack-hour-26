6bd6c2169b0f0c61363ec7f0a2555961d2c4d934
joel burton <joeltburton@gmail.com>
2019-02-23 09:28:24 -0800

    implement bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -3,9 +3,34 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 
-
 function bubbleSort(array) {
+  const sorted = array.slice();
+  let { length } = array;
 
+  array.forEach(() => {
+    let temp;
+    let i = 0;
+    let j = 1;
+
+    while (i < length - 1) {
+      // swap and increment
+      if (sorted[i] > sorted[j]) {
+        temp = sorted[i];
+        sorted[i] = sorted[j];
+        sorted[j] = temp;
+      }
+      i += 1;
+      j += 1;
+    }
+
+    length -= 1;
+  });
+  return sorted;
 }
+
+const arr = [9, 4, 7, 2, 8, 4, 2, 1, 6];
+const sorted = bubbleSort(arr);
+console.log('sorted:', sorted);
+
 
 module.exports = bubbleSort;
