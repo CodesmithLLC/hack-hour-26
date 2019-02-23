09bcbe299406ee78bf25f270a47ebb2fa67a84fa
Chang Yea Moon <changyeamoon@gmail.com>
2019-02-23 09:41:54 -0800

    bubble sorted

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -3,9 +3,20 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 
-
 function bubbleSort(array) {
-
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = 0; j < array.length - 1 - i; j += 1) {
+      if (array[j + 1] < array[j]) {
+        const temp = array[j + 1];
+        array[j + 1] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
 }
 
 module.exports = bubbleSort;
+
+// const arr = [5, 1, 4, 2, 8, 9];
+// bubbleSort(arr);
+// console.log(arr);
