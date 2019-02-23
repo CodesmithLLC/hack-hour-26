1e53969e3a0b34b47de9b065a6067d3ed51ee106
JayCogen44 <jaycog44@gmail.com>
2019-02-23 09:32:29 -0800

    bubble sorted

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,22 @@
 
 
 function bubbleSort(array) {
-
+    let sorted = false;
+    while (sorted == false) {
+        sorted = true;
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] && array[i] > array[i + 1]) {
+                const temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                sorted = false;
+            }
+        }
+    }
+    return array;
 }
+
+// const a = [3, 1, 7, 4, 9, 5, 2, 6, 0];
+// console.log(bubbleSort(a));
 
 module.exports = bubbleSort;
