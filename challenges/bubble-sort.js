418ab8553ff260e5181849e9f1e94869027ace00
jordanzobean <jordanbetzer@gmail.com>
2019-02-23 09:39:30 -0800

    finished bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -2,10 +2,41 @@
 
 // https://en.wikipedia.org/wiki/Bubble_sort
 
-
-
 function bubbleSort(array) {
+  // check for valid input
+  if (!Array.isArray(array) || array.length === 0) return undefined;
+  // declare var to track if array is sorted
+  let elementsOutOfOrder = 0;
 
+  // initial check for out of order elements
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < array[i - 1]) {
+      elementsOutOfOrder += 1;
+    }
+  }
+  if (elementsOutOfOrder === 0) return array;
+
+  while (elementsOutOfOrder > 0) {
+    // loop over the array the check if it's sorted
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < array[i - 1]) {
+        elementsOutOfOrder += 1;
+      } else {
+        elementsOutOfOrder -= 1;
+      }
+    }
+    // loop over array and swap elements out of order
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < array[i - 1]) {
+        let temp = array[i - 1];
+        array[i - 1] = array[i];
+        array[i] = temp;
+      }
+    }
+  }
+  return array;
 }
+
+console.log(bubbleSort([5, 4, 3, 2, 1]));
 
 module.exports = bubbleSort;
