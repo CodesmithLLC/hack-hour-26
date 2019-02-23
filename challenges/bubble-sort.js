4f081ee8d487c953ed95de6c75a82702fdafcdd3
trischoe <tristans7@gmail.com>
2019-02-23 09:19:09 -0800

    bubble-sort approach

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,6 +5,21 @@
 
 
 function bubbleSort(array) {
+    let swapped = true;
+
+    while(swapped){
+        swapped = false;
+
+        for(let i = 1; i < array.length; i++){
+            if(array[i-1] > array[i]){
+               let temp = array[i-1];
+               array[i-1] = array[i];
+               array[i] = temp;
+              swapped = true;
+            }
+        }
+    }
+    return array;
 
 }
 
