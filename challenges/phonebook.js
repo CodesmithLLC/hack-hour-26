273cd6264c5d24bdca1ae33306e9b8e296a57da8
Byron Inocencio <jay.byron@gmail.com>
2019-02-25 09:48:48 -0800

    kinda finished phonebook

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -27,12 +27,58 @@
 //  return the number associated with the name in the jazbook
 function findName(jazbook, name) {
 
+  for(let i =0; i< jazbook.length; i++){
+    if(jazbook[i][0] === name){
+      return jazbook[i][1];
+    }
+  }
+
+  return false;
 }
 
 // return an object literal representing the jazbook
 function makePhoneBookObject(jazbook){
 
+  // to hold all our entries
+  this.phonebook = {};
+  // populates the phonebook
+  for (let i=0; i< jazbook.length; i++) {
+    this.phonebook[jazbook[i][0]] = jazbook[i][1];
+  }
+
+  this.add = (name,number) => {
+    
+    this.phonebook[name] = number;
+
+  }
+
+  this.lookUp = (name) => {
+
+    return this.phonebook[name];
+
+  }
+
+  this.remove = (name) =>{
+
+    if(this.phonebook[name]){
+      delete this.phonebook[name];
+    }
+
+  }
+
 }
+
+
+
+jazbook = [['alex','301-844-3421'],
+     ['jae','301-844-1211'],
+     ['david','301-844-0978'],
+     ['travis','301-844-8505'],
+     ['jasmine','1800-974-4539'],
+   ];
+
+makePhoneBookObject(jazbook);
+
 
 const objectToExport = {
   findName,
--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -11,6 +11,17 @@
  */
 function uniqueNumber(array) {
 
-}
+  let sum =0;
+  for(let i=0; i< array.length; i++){
+    sum += array[i];
+  }
 
+  for(let j=0; j<array.length; j++){
+    sum =- array[j];
+  }
+
+  return sum;
+
+}
+console.log(uniqueNumber([1,2,1,3,3]));
 module.exports = uniqueNumber;
