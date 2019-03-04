ac86fb3f0620cc51460d649140d2314362462e28
JayCogen44 <jaycog44@gmail.com>
2019-03-04 12:06:18 -0800

    count stairs - done

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -11,11 +11,37 @@
  * 2 + 1 + 2
  * 2 + 2 + 1
  *
+ * n === 3
+ * 1 1 1
+ * 2 1
+ * 1 2
+ * 
+ * n === 4
+ * 1 1 1 1
+ * 2 1 1
+ * 1 2 1
+ * 1 1 2
+ * 2 2 
+ * 
+ * 
  * That is a total of 8 different ways to take 5 steps, given that you can take 1 or 2 steps at a time.
  */
 
 function countStairs(n) {
-
+    let count = 0;
+    function rFunc(stairsLeft) {
+        if (stairsLeft === 0) {
+            count++;
+            return;
+        }
+        if (stairsLeft >= 1) rFunc(stairsLeft - 1);
+        if (stairsLeft >= 2) rFunc(stairsLeft - 2);
+        return;
+    }
+    rFunc(n);
+    return count;
 }
+
+// console.log(countStairs(4));
 
 module.exports = countStairs;
