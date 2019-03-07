706d5b0ad57fb45445ba10c3ff67cdcc1fb70383
trischoe <tristans7@gmail.com>
2019-03-07 09:34:42 -0800

    doubly-linked list approach

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,40 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+    const node = new Node(val);
+
+    if (!this.head){
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.prev = this.tail;
+      this.tail = node;
+    }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  let cur = this.head;
+  while(cur){
+    if(cur.val === val){
+      if(cur === this.head && cur === this.tail){
+        this.head = null;
+        this.tail = null;
+      } else if(cur === this.head) {
+        this.head = this.head.next;
+        this.head.prev = null;
+      } else if(cur === this.tail) {
+        this.tail = this.tail.prev;
+        this.tail.next = null;
+      } else {
+        cur.prev.next = cur.next;
+        cur.next.prev = cur.prev;
+      }
+      cur = cur.next;
+    }
+  }
 };
 
 module.exports = LinkedList;
