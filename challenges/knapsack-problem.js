9d6b4bc5c1f9f3cd1e3c327fb9459e0a7754226c
JayCogen44 <jaycog44@gmail.com>
2019-03-08 09:29:45 -0800

    filled my knapsack

--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -10,7 +10,21 @@
 */
 
 function solveKnapsack(items, weightAvailable) {
-
+  const sortedItems = items.sort((a, b) => a.weight > b.weight);
+  let result = 0;
+  let workingWeight = weightAvailable;
+  for (let i = 0; i < sortedItems.length; i++) {
+    const cur = sortedItems[i];
+    if (workingWeight - cur.weight >= 0) {
+      result += cur.value;
+      workingWeight -= cur.weight;
+    }
+  }
+  return result;
 };
+
+items = [{ weight: 1, value: 3 }, { weight: 2, value: 4 }, { weight: 3, value: 5 }];
+console.log(solveKnapsack(items, 3)); // returns 7 (from items[0] and items[1])
+
 
 module.exports = solveKnapsack;
