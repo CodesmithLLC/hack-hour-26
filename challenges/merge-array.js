d44e19a4b0b4b1bf1e09f74b4cf2299d46995951
Harmon Huynh <harmonh@gmail.com>
2019-05-08 16:57:08 -0700

    Completed merge-array in O(n) time

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -7,17 +7,37 @@
  * var another_array = [1,5,8,12,14,19];
  *
  * mergeArrays(my_array, another_array); -> [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21]
- * 
- * BONUS: 
- * Complete in O(n) time  
+ *
+ * BONUS:
+ * Complete in O(n) time
  *
  */
 
 function mergeArrays(arr1, arr2) {
-  const newArray = arr1.concat(arr2);
-  return newArray.sort((a, b) => {
-    return a - b;
-  })
+  let sortedArray = [];
+  let count1 = 0;
+  let count2 = 0;
+
+  while (arr1[count1] && arr2[count2]) {
+    if (arr1[count1] < arr2[count2]) {
+      sortedArray.push(arr1[count1]);
+      count1 += 1;
+    } else {
+      sortedArray.push(arr2[count2]);
+      count2 += 1;
+    }
+  }
+  if (arr1[count1] === undefined) {
+    sortedArray.concat(arr2.slice(count2));
+  }
+  if (arr2[count2] === undefined) {
+    sortedArray = sortedArray.concat(arr1.slice(count1));
+  }
+  return sortedArray;
 }
+let my_array = [3, 4, 6, 10, 11, 15, 21];
+let another_array = [1, 5, 8, 12, 14, 19];
+
+console.log('result', mergeArrays(my_array, another_array));
 
 module.exports = mergeArrays;
